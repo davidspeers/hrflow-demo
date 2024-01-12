486682c41d33e52f27019096ec0d9b9936acd584
David Speers <speerscd@gmail.com>
2024-01-12 14:40:38 +0100

    add path aliases

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
+import App from "@components/App/App.jsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App/App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
+    },
+  },
   plugins: [react()],
-})
+});
