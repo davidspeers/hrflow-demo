0dd1dcbaa94b9a8b84e42af0fb4a18cbcb1902d5
David Speers <speerscd@gmail.com>
2024-01-12 14:40:47 +0100

    remove comments from test

--- src/components/Header/Header.test.jsx
+++ src/components/Header/Header.test.jsx
@@ -13,14 +13,11 @@ test("toggles mobile navigation menu when button is clicked", () => {
   const menuButton = screen.getByRole("button", { name: "" });
   expect(menuButton).not.toBeNull();
 
-  // Check initial state
   let mobileNav = screen.queryByRole("navigation");
   expect(mobileNav).toBeNull();
 
-  // Click the button
   fireEvent.click(menuButton);
 
-  // Check that the mobile navigation is now visible
   mobileNav = screen.getByRole("navigation");
   expect(mobileNav).not.toBeNull();
 });
